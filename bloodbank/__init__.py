b09d69051d11bbc58acb484141d193666473bcd6
Pia Francesca <pia.rissom@gmail.com>
2020-12-28 17:35:35 +0100

    add initial project files

---
+++ bloodbank/__init__.py
@@ -0,0 +1,28 @@
+import os
+
+from flask import Flask
+
+
+def create_app():
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'bloodbank.sqlite'),
+    )
+
+     # load the instance config, if it exists, when not testing
+    app.config.from_pyfile('config.py', silent=True)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+
+    return app
