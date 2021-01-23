e8296ee909afefb0cb0b96bba5d6bc7a501f3678
Pia Francesca <pia.rissom@gmail.com>
2021-01-23 13:25:19 +0100

    made bloodbank_id a foreign key

--- bloodbank/schema.sql
+++ bloodbank/schema.sql
@@ -4,25 +4,27 @@ DROP TABLE IF EXISTS bloodstock;
 
 CREATE TABLE bloodbank (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT UNIQUE NOT NULL
+    name VARCHAR(25) UNIQUE NOT NULL
 );
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  username TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL,
-  bloodbank_id INTEGER NOT NULL
+  username VARCHAR(30) UNIQUE NOT NULL,
+  password VARCHAR(30) NOT NULL,
+  bloodbank_id INTEGER NOT NULL,
+  FOREIGN KEY (bloodbank_id) REFERENCES bloodbank (id)
 );
 
 
 CREATE TABLE bloodstock (
-  id INTEGER PRIMARY KEY,
-  blood_type TEXT NOT NULL,
-  blood_group TEXT NOT NULL,
-  rhesus TEXT NOT NULL,
+  id INTEGER(5) PRIMARY KEY,
+  blood_type VARCHAR(25) NOT NULL,
+  blood_group VARCHAR(2) NOT NULL,
+  rhesus CHAR(1) NOT NULL,
   created TEXT NOT NULL,
-  room INTEGER,
-  fridge INTEGER,
-  shelf INTEGER,
-  bloodbank_id INTEGER NOT NULL
+  room VARCHAR(10),
+  fridge VARCHAR(10),
+  shelf VARCHAR(10),
+  bloodbank_id INTEGER NOT NULL,
+  FOREIGN KEY (bloodbank_id) REFERENCES bloodbank (id)
 );
